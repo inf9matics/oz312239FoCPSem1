b3ee9d25db6b1810073be2cfdcfe6f3d6b24e761
loonek <28lunek@wp.pl>
2023-10-20 12:10:00 +0200

    task2 lab2

---
+++ lab-02/task2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+void multiply(){
+    x = a * b;
+    std::cout << x;
+}
+
+main(){
+    auto x, b, a;
+    char t;
+    std::cout<< "multiply = m\n" << "divise\n" << "add = a\n" << "subtract = s\n";
+    std::cin >> t;
+    std::cout << "a = ";
+    std::cin >> a;
+    std::cout <<"b = ";
+    std::cin >>
+    if(t == "m"){
+        multiply(auto = x)
+    }
+    return x;
+}
