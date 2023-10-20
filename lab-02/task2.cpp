8c3d676d07d6ae13bd331abf8a4fc2a122511227
loonek <28lunek@wp.pl>
2023-10-20 12:46:11 +0200

    task2

---
+++ lab-02/task2.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+double x, b, a;
+
+void multiply(){
+    x = a * b;
+}
+
+void divise(){
+    if(a != 0, b != 0){
+        x = a / b;
+    }
+    else{
+        std::cout<<"One of the numbers equals 0\n";
+    }
+}
+
+void add(){
+    x = a + b;
+}
+
+void subtract(){
+    x = a - b;
+}
+
+main(){
+    int t;
+    std::cout<< "multiply = 1\n" << "divise = 2\n" << "add = 3\n" << "subtract = 4\n";
+    std::cin >> t;
+    std::cout << "a = ";
+    std::cin >> a;
+    std::cout <<"b = ";
+    std::cin >> b;
+    if(t == 1){
+        multiply();
+    }
+    if(t == 2){
+        divise();
+    }
+    if(t == 3){
+        add();
+    }
+    if(t == 4){
+        subtract();
+    }
+    std::cout<<x;
+    return 0;
+}
