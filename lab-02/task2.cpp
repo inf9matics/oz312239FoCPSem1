4556d64a55e9c1a8c6afbab3958625650167c990
loonek <28lunek@wp.pl>
2023-10-24 16:31:20 +0200

    all tasks for friday classes

--- lab-02/task2.cpp
+++ lab-02/task2.cpp
@@ -1,48 +1,22 @@
 #include<iostream>
 
-double x, b, a;
-
-void multiply(){
-    x = a * b;
-}
-
-void divise(){
-    if(a != 0, b != 0){
-        x = a / b;
+main(){
+    double a = 0;
+    double b = 0;                               //double is used for every number so answer will show correctly
+    std::cout<<"a = ";
+    std::cin>>a;
+    std::cout<<"b = ";
+    std::cin>>b;
+    if(a==0 && b!=0){
+        std::cout<<"a == 0!";
     }
     else{
-        std::cout<<"One of the numbers equals 0\n";
+        if(a == b && a == 0){
+            std::cout<<"Every real number";
+        }
+        else{
+            std::cout<<"x = "<<-b/a;
+        }
     }
-}
-
-void add(){
-    x = a + b;
-}
-
-void subtract(){
-    x = a - b;
-}
-
-int main(){
-    int t;
-    std::cout<< "multiply = 1\n" << "divise = 2\n" << "add = 3\n" << "subtract = 4\n";
-    std::cin >> t;
-    std::cout << "a = ";
-    std::cin >> a;
-    std::cout <<"b = ";
-    std::cin >> b;
-    if(t == 1){
-        multiply();
-    }
-    if(t == 2){
-        divise();
-    }
-    if(t == 3){
-        add();
-    }
-    if(t == 4){
-        subtract();
-    }
-    std::cout<<x;
     return 0;
 }
