94d1f554d7c6926de94911dc97986c53106034aa
loonek <28lunek@wp.pl>
2023-11-03 14:05:28 +0100

    i'll try to do rest of exercises later

--- lab-04/Task4.cpp
+++ lab-04/Task4.cpp
@@ -3,6 +3,7 @@
 #include<chrono>  
 #include<limits>
 #include<iomanip>
+#include<fstream>
 
 double get_random_in_range(double from = std::numeric_limits<double>::min(), double to = std::numeric_limits<double>::max())
 {
@@ -17,17 +18,37 @@ int main(){
     std::cin>> R;
     std::cout<<"Columns = ";
     std::cin>> C;
+    if(R>20 || C>20){
+        std::cout<<"Array shouldn't be bigger than 20x20";
+        return 0;
+    }
     double tab[R][C];
+    double trans[C][R];
+    std::fstream file;              //couldn't fit it in terminal
+    file.open("Task4.txt");
     for(int i = 0; i < R; i++){
         for(int j = 0; j < C; j++){
             tab[i][j] = get_random_in_range(-100.0,100.0);
         }
     }
+    file<<std::endl<<"Regular matrix: "<<std::endl;
     for (int i = 0; i < R; i++){
-        for(int j = 0; j > C; j++){
-            std::cout<< std::setw(5) << tab[i][j];   
+        for(int j = 0; j < C; j++){
+            file << std::setw(14) << std::fixed << std::setprecision(9) << tab[i][j];               //had to change width to over 12, because number mashed up together  
         }
-        std::cout<<std::endl;
+        file <<std::endl<<std::endl;
     }
+    for (int i = 0; i < R; ++i)
+      for (int j = 0; j < C; ++j) {
+         trans[j][i] = tab[i][j];
+    }
+    file<<std::endl<<"Transpositioned matrix: "<<std::endl;
+    for (int i = 0; i < C; i++){
+        for(int j = 0; j < R; j++){
+            file << std::setw(14) << std::fixed << std::setprecision(9) << trans[i][j];
+        }
+        file <<std::endl<<std::endl;
+    }
+    file.close();
     return 0;
 }
