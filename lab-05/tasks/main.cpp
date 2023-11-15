241cc61b58c6819b4ea7a0629537e181c7fbb8b6
lasotek <lasotek@gmail.com>
2023-11-15 22:12:51 +0100

    Feedback! Wrong!

--- lab-05/tasks/main.cpp
+++ lab-05/tasks/main.cpp
@@ -28,3 +28,23 @@ int main(){
             break;
     }
 }
+
+// As you don't know how to write multi-module programs in C++ yet, don't show off your ignorance.
+// It is not yet required.
+// The program has a fatal structure.
+// Never define a function body in a header file.
+// Only a function declaration can be included in the header.
+// Function templates can be confusing. They are not an implementation, only a declaration, with abstract types.
+// Implementation happens only when abstract types become actual types.
+// Function bodies must be in *.cpp files.
+// Header files should have a #pragma once, or #ifndef ... declaration. #endif, which protect
+// from including the same file multiple times. Your program compiles only because you
+// you do not insert anything via #include more than once.
+
+// The functions themselves are also of bad quality in your program. Almost all of the type:
+//  void f() { ... }
+//  With no parameters and no result. This is the wrong approach.
+//  You simply cut the alleged main() into independent pieces corresponding to individual tasks.
+//  With this approach, there was no chance for recursive implementations of algorithms.
+
+// If it was a project program it would be definitely rejected!!!!!!!!!!!!!!!!
--- lab-05/tasks/new_tasks.h
+++ lab-05/tasks/new_tasks.h
@@ -1,66 +1,86 @@
-#include<iostream>
+#include <iostream>
 
-void prime(){
+//Awful. Does not return anything. Does not accept any parameters.
+void prime()
+{
     int x = 0;
     bool prime = true;
-    std::cout<<"Provide a positive number = ";
-    std::cin>>x;
-    if (x == 0 || x == 1) {
-    prime = false;
+    std::cout << "Provide a positive number = ";
+    std::cin >> x;
+    if (x == 0 || x == 1)
+    {
+        prime = false;
     }
-    for (int i = 2; i <= x/2; i++){
-        if (x % i == 0){
+    // for (int i = 2; i <= x / 2; i++) // too many iterations
+    for (int i = 2; i * i <= x; i++)
+    {
+        if (x % i == 0)
+        {
             prime = false;
             break;
-            }
+        }
     }
-    if(prime){
-        std::cout<<x <<" is a prime number\n";
+    if (prime)
+    {
+        std::cout << x << " is a prime number\n";
     }
-    else{
-        std::cout<<x <<" is not a prime number\n";
+    else
+    {
+        std::cout << x << " is not a prime number\n";
     }
 }
 
-void sieve(){
+// This is no Eratosthenes sieve at all!!!!!!!!!!!!!!!!!!!
+void sieve()
+{
     int x = 0;
-    std::cout<<"Provide a positive number = ";
-    std::cin>>x;
-    for (int i = 2; i <= x/2; i++){
-        if (x % i == 0){
-            std::cout<<i<<std::endl;
-            }
-    }
-}
-
-void gcd(){
-    int a = 0,b = 0;
-    std::cout<<"a = ";
-    std::cin>>a;
-    std::cout<<"b = ";
-    std::cin>>b;
-    if ( a > b) {   
-    std::swap(a,b);
-    }
-    for (int i = a; i !=  0; i--) {
-        if (a % i == 0 && b % i ==0) {
-        std::cout<<i;
-        break;
+    std::cout << "Provide a positive number = ";
+    std::cin >> x;
+    for (int i = 2; i <= x / 2; i++)
+    {
+        if (x % i == 0)
+        {
+            std::cout << i << std::endl;
         }
     }
 }
 
-void power(){
-    int b = 0, e = 0;
-    std::cout<< "Provide base = ";
-    std::cin>> b;
-    std::cout<< "Provide exponent = ";
-    std::cin>> e;
-    int r = b;
-    for(int i = 0; i < e; i++){
-        r=r*b;
+// Bad implementation. Learn Euclid's algorithm.
+void gcd()
+{
+    int a = 0, b = 0;
+    std::cout << "a = ";
+    std::cin >> a;
+    std::cout << "b = ";
+    std::cin >> b;
+    if (a > b)
+    {
+        std::swap(a, b);
     }
-    std::cout<< b<< "^"<< e<< " = "<<r;
+    for (int i = a; i != 0; i--)
+    {
+        if (a % i == 0 && b % i == 0)
+        {
+            std::cout << i;
+            break;
+        }
+    }
+}
+
+//What about negative exponents?
+void power()
+{
+    int b = 0, e = 0;
+    std::cout << "Provide base = ";
+    std::cin >> b;
+    std::cout << "Provide exponent = ";
+    std::cin >> e;
+    int r = b;
+    for (int i = 0; i < e; i++)
+    {
+        r = r * b;
+    }
+    std::cout << b << "^" << e << " = " << r;
 }
 
 int ite(int n)
@@ -68,7 +88,8 @@ int ite(int n)
     int x1 = 0, x2 = 1, x3;
     if (n == 0)
         return x1;
-    for (int i = 2; i <= n; i++) {
+    for (int i = 2; i <= n; i++)
+    {
         x3 = x1 + x2;
         x1 = x2;
         x2 = x3;
@@ -83,46 +104,49 @@ int rec(int n)
     return rec(n - 1) + rec(n - 2);
 }
 
-int new_tasks(){
-     std::cout<< "Choose a task:\n"
-             << "[1] Function to check if a number is prime\n"
-             << "[2] Sieve of Eratosthenes\n"
-             << "[3] Function to determine the greatest common divider\n"
-             << "[4] Function to find an integer power of a real number\n"
-             << "[5] Function that determines a given Fibonacci number\n";
-    
+int new_tasks()
+{
+    std::cout << "Choose a task:\n"
+              << "[1] Function to check if a number is prime\n"
+              << "[2] Sieve of Eratosthenes\n"
+              << "[3] Function to determine the greatest common divider\n"
+              << "[4] Function to find an integer power of a real number\n"
+              << "[5] Function that determines a given Fibonacci number\n";
+
     int s = 0;
-    std::cin>>s;
-    switch(s){
+    std::cin >> s;
+    switch (s)
+    {
+    case 1:
+        prime();
+        break;
+    case 2:
+        sieve();
+        break;
+    case 3:
+        gcd();
+        break;
+    case 4:
+        power();
+        break;
+    case 5:
+        int n = 0;
+        std::cout << "n = ";
+        std::cin >> n;
+        std::cout << "[1] Iteratively\n"
+                  << "[2] Recursively\n";
+        int t = 0;
+        std::cin >> t;
+        switch (t)
+        {
         case 1:
-            prime();
+            std::cout << ite(n);
             break;
         case 2:
-            sieve();
+            std::cout << rec(n);
             break;
-        case 3:
-            gcd();
-            break;
-        case 4:
-            power();
-            break;
-        case 5:
-            int n = 0;
-            std::cout<< "n = ";
-            std::cin>> n;
-            std::cout<< "[1] Iteratively\n"
-                     << "[2] Recursively\n";
-            int t = 0;
-            std::cin>>t;
-            switch(t){
-                case 1:
-                    std::cout<<ite(n);
-                    break;
-                case 2:
-                    std::cout<<rec(n);
-                    break;
-            }
-            break;       
+        }
+        break;
     }
     return 0;
 }
